3d34c78300e80a74d05913f0e873ac5c4ef9d9a7
Jordi Martínez Frias <jordimart83@gmail.com>
2017-10-09 19:22:37 +0200

    [ADD] HTTP-POST Creando un nuevo registro en FireBase.

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { Router } from '@angular/router';
 import { Heroe } from "../../interfaces/heroe.interface";
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -9,19 +11,27 @@ import { Heroe } from "../../interfaces/heroe.interface";
 })
 export class HeroeComponent implements OnInit {
 
-heroe:Heroe = {
-  nombre:"",
-  bio:"",
-  casa:"Marvel"
+  heroe: Heroe = {
+    nombre: "",
+    bio: "",
+    casa: "Marvel"
 
-}
-  constructor() { }
+  }
+  constructor(private _heroesservice: HeroesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
-  guardar(){
+  guardar() {
     console.log(this.heroe);
+
+    this._heroesservice.nuevoHeroe(this.heroe)
+      .subscribe(data => {
+        this.router.navigate(['/heroe',data.name])
+      },
+      error => console.error(error))
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Heroe } from '../interfaces/heroe.interface';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HeroesService {
 
-  constructor() { }
+  heroesURL:string = "https://heroesapp-8952c.firebaseio.com/heroes.json"
+
+  constructor(
+    private http:Http
+  ) { }
+
+  nuevoHeroe( heroe:Heroe ){
+
+    let body = JSON.stringify( heroe );
+    let headers = new Headers({
+      'Content-Type':'application/json'
+    })
+
+    return this.http.post( this.heroesURL, body, { headers } )
+      .map( res=>{
+        console.log(res.json());
+        return res.json();
+      })
+
+  }
 
 }
